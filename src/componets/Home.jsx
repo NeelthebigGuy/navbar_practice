fffb2194e77fa8fc981e77474c43e9d65f16ba65
NeelthebigGuy <nkhiroya33@gmail.com>
2024-01-30 16:59:38 -0400

    updated with homepage

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './css/home.css'
 
+
+function HomeContent(props){
+  return(
+    <>
+    </>
+  )
+}
+
+
 export default function Home() {
   return (
-    <>
+    <> 
+    {/* Title */}
     <div className='mycontainer'>
         <div className=''>
           <h1 className='mytitle bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text'>
@@ -16,16 +26,33 @@ export default function Home() {
       Third year computer science student
     </div>
 
+    {/* Content */}
+
     <div className='contentcontainer'>
-      <div className='contenttitle'>
+      <div className='contenttitle pt-12'>
         Welcome to my portfolio
+        <hr className='mt-2'/>
       </div>
     </div>
 
     <div className='contentcontainer'>
-        <div className=''>
-          About me
-        </div>
+        <span>
+          <div className='homecontenttitle pt-10'>
+            About me
+          </div>
+
+          <div className='homecontent pl-4 pr-4 pb-10'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus accusantium minima maxime reprehenderit? Nobis, impedit deserunt reiciendis obcaecati illum, vitae cum incidunt officiis delectus vero minus id pariatur, maxime perferendis?
+          </div>
+
+          <div className='homecontenttitle pt-10'>
+            Education
+          </div>
+
+          <div className='homecontent pl-4 pr-4 pb-10'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus accusantium minima maxime reprehenderit? Nobis, impedit deserunt reiciendis obcaecati illum, vitae cum incidunt officiis delectus vero minus id pariatur, maxime perferendis?
+          </div>
+        </span>
     </div>
     </>
   )
